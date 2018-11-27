5878e25070d8f09ec243f7196de15715f4518a9b
Ola <aleksandra.burchart@gmail.com>
2018-11-27 18:44:06 +0100

    PEP 8

---
+++ 1.7klasyfikator.py
@@ -0,0 +1,9 @@
+def klasyfikator(napis):
+    for slowo in napis.split():
+        if len(slowo)> 5:
+            print('- ' + slowo)
+        else:
+            print('+ ' + slowo)
+
+
+klasyfikator("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
